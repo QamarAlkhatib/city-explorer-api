5e90dd99633ed051bfe5cbc59c0723adc2cd7391
Qamar <qalkhatib0@gmail.com>
2021-09-22 02:35:46 +0300

    edits

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // to use the required libraries same as importing
 const express = require('express');
 require('dotenv').config();
+const axios = require('axios');
 const cors = require('cors');
 const server = express();
 const weatherData = require('./data/weather.json');
@@ -10,48 +11,46 @@ const PORT = process.env.PORT;
 server.use(cors());
 
 
+server.get('/', homeRouteHandler);
+server.get('/weather', weatherRouteHandler);
+server.get('*', notFoundHandler);
 
-class Forecast {
-    constructor(date, des, highTemp, lowTemp, cityname) {
-        this.date = date;
-        this.desc = des;
-        this.highTemp = highTemp;
-        this.lowTemp = lowTemp;
-        this.cityname = cityname
-    }
+
+function homeRouteHandler(req, res) {
+    res.send('home Route');
 }
 
-// localhost:3007/weather?cityname=Seattle&timezone=America/Los_Angeles&description=Light rain&date=2021-03-25
-// localhost:3007/weather?cityname=Seattle&timezone=America/Los_Angeles
+// localhost:3007/weather?city=amman&key=2838128258f14da4a8b2d5064dd19e7c
+function weatherRouteHandler(req, res){
+    // res.send('weather route')
+    let cityNameData = req.query.city;
+    let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityNameData}&key=${process.env.WEATHER_API_KEY}`;
+    console.log(weatherURL);
+    
 
-server.get('/Weather', (req, res) => {
-
-    let cityNameData = req.query.cityname;
-    console.log(req.query);
-    console.log(req.query.cityname);
-
-
-
-    let weatherInfo = weatherData.find((item) => {
-
-        if (item.city_name === cityNameData) {
-            return item;
-        }
-
+    axios.get(weatherURL).then(weatherDataResult => {
+        console.log('inside sending request');
+        let newArray = weatherDataResult.data.map(element => {
+            res.send(newArray);
+            return new Forecast(element);
+        });
+        console.log(newArray);
+    }).catch(error => {
+        console.log("error in sending the data ", error)
     });
-    let newArray = weatherInfo.data.map(element => {
-        return new Forecast(element.datetime, element.weather.description, element.high_temp, element.low_temp, element.city_name);
-    });
-    console.log(newArray);
-    console.log('weather Info', weatherInfo);
-    res.send(newArray);
-});
 
+}
 
-
-server.get('*', (req, res) => {
+function notFoundHandler(req, res) {
     res.status(404).send('Route is not found')
-})
+}
+
+class Forecast {
+    constructor(element) {
+        this.date = element.datetime;
+        this.description = element.weather.description;
+    }
+}
 
 server.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
